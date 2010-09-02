03ea8efdb30f7c7d276f56322a0d63bc4565d694
Jake Donham <jake@donham.org>
2010-09-02 18:53:29 -0400

    better piece rotation

--- puzzle.ml
+++ puzzle.ml
@@ -63,10 +63,10 @@ let onload () =
            | "mousedown" when not e#_get_shiftKey ->
                Fd.mouseEvent "mousemove" canvas |>
                F.collect_e
-               (fun ((x, y), _) e ->
-                  let x' = e#_get_clientX and y' = e#_get_clientY in
-                  ((x', y'), (x' - x, y' - y)))
-               ((e#_get_clientX, e#_get_clientY), (0, 0)) |>
+                 (fun ((x, y), _) e ->
+                    let x' = e#_get_clientX and y' = e#_get_clientY in
+                    ((x', y'), (x' - x, y' - y)))
+                 ((e#_get_clientX, e#_get_clientY), (0, 0)) |>
                F.map (fun (_, d) -> d)
            | _ -> F.never) |>
     F.join_e |>
@@ -119,30 +119,36 @@ let onload () =
       (fun i ->
          let (e, _) = shape_events.(i + 1) in
 
-         (* XXX this doesn't track the current angle between drags *)
-         (* do it with deltas, but take care not accumulate error *)
+         let xy = xy_of_drag e (Random.int 1024, Random.int 512) in
+
          let angle =
            e |>
            F.map
              (fun e ->
                 match e#_get_type with
                   | "mousedown" when e#_get_shiftKey ->
+                      let (cx, cy) = F.sample xy in
                       let x = e#_get_clientX and y = e#_get_clientY in
+                      let a = atan2 (float_of_int (x - cx)) (float_of_int (y - cy)) in
                       Fd.mouseEvent "mousemove" canvas |>
-                      F.map
-                        (fun e ->
-                           let x' = e#_get_clientX and y' = e#_get_clientY in
-                           atan2 (float_of_int (x' - x)) (float_of_int (y' - y)))
+                      F.collect_e
+                        (fun (td, _) e ->
+                           let x = e#_get_clientX and y = e#_get_clientY in
+                           let a = atan2 (float_of_int (x - cx)) (float_of_int (y - cy)) in
+                           let d = a -. td in
+                           (td +. d, d))
+                        (a, 0.) |>
+                      F.map (fun (_, d) -> d)
                   | _ -> F.never) |>
            F.join_e |>
-           F.hold 0. in
+           F.collect_b (fun a d -> a +. d) 0. in
 
          {
            id = i + 1;
            w = Random.int 128;
            h = Random.int 128;
            color = Fda.color ~a:(Random.int 256) (Random.int 256) (Random.int 256) (Random.int 256);
-           xy = xy_of_drag e (Random.int 1024, Random.int 512);
+           xy = xy;
            angle = angle;
          }) in
 
@@ -160,10 +166,10 @@ let onload () =
             F.bind2 s.xy s.angle begin fun (x, y) a ->
               F.return
                 (fun ctx ->
-                   ctx#translate (float_of_int (px + x + s.w/2)) (float_of_int (py + y + s.h/2));
+                   ctx#translate (float_of_int (px + x)) (float_of_int (py + y));
                    ctx#rotate (-. a);
-                   ctx#translate (float_of_int (-px -x - s.w/2)) (float_of_int (-py -y - s.h/2));
-                   Fda.fillRect (float_of_int (px + x), float_of_int (py + y)) (float_of_int s.w) (float_of_int s.h) (color s) ctx)
+                   ctx#translate (float_of_int (-px -x)) (float_of_int (-py -y));
+                   Fda.fillRect (float_of_int (px + x - s.w/2), float_of_int (py + y - s.h/2)) (float_of_int s.w) (float_of_int s.h) (color s) ctx)
             end)
          (Array.to_list shapes))
       F.return
